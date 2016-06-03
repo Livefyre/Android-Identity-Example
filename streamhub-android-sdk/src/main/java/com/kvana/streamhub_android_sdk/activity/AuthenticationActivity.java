971b2bd482fab2f9094f2e184acd6ad3b24ac2c5
HariKvana <harikvana@gmail.com>
2016-06-03 18:55:33 +0530

    BaseActivity moved to activity package

--- streamhub-android-sdk/src/main/java/com/kvana/streamhub_android_sdk/activity/AuthenticationActivity.java
+++ streamhub-android-sdk/src/main/java/com/kvana/streamhub_android_sdk/activity/AuthenticationActivity.java
@@ -8,7 +8,6 @@ import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-import com.kvana.streamhub_android_sdk.BaseActivity;
 import com.kvana.streamhub_android_sdk.R;
 import com.kvana.streamhub_android_sdk.network.RetrofitHandler;
 import com.kvana.streamhub_android_sdk.util.Constant;
