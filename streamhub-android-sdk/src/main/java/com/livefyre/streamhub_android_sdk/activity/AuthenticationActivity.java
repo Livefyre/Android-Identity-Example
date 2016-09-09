cde1e999ef16674a54cec682ef8f6f74489c0ab1
kris9 mac42016 <kris9@kris9s-MacBook-Pro.local>
2016-09-08 23:10:15 -0700

    updated flow to after make authenticate call after last redirect to http://....lfep2-comments.html#

--- streamhub-android-sdk/src/main/java/com/livefyre/streamhub_android_sdk/activity/AuthenticationActivity.java
+++ streamhub-android-sdk/src/main/java/com/livefyre/streamhub_android_sdk/activity/AuthenticationActivity.java
@@ -68,6 +68,19 @@ public class AuthenticationActivity extends BaseActivity {
                 } catch (UnsupportedEncodingException e) {
                     e.printStackTrace();
                 }
+            } else if (url.equals("http://livefyre-cdn-dev.s3.amazonaws.com/demos/lfep2-comments.html#")){
+                try {
+                    showProgressDialog();
+                    AuthenticationClient.authenticate(
+                            environment,
+                            LivefyreConfig.origin,
+                            LivefyreConfig.referer,
+                            CookieManager.getInstance().getCookie(URL),
+                            new AuthCallback());
+                    webview.setVisibility(View.GONE);
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
             }
             return false;
         }
