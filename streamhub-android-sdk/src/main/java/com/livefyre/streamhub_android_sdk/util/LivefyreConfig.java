7f1fd8d163be75470e2e8f8f9aa126a34006c2f3
ramesh <ramesh.kvana@gmail.com>
2016-11-01 20:36:47 +0530

    LF-23718-APIs Connect to Livefyre Servers over HTTPS by Default

--- streamhub-android-sdk/src/main/java/com/livefyre/streamhub_android_sdk/util/LivefyreConfig.java
+++ streamhub-android-sdk/src/main/java/com/livefyre/streamhub_android_sdk/util/LivefyreConfig.java
@@ -2,15 +2,15 @@ package com.livefyre.streamhub_android_sdk.util;
 
 public class LivefyreConfig {
     // comments
-    public static String scheme = "http";
+    public static String scheme = "https";
     public static String environment = "livefyre.com";
     public static String bootstrapDomain = "bootstrap";
     public static String quillDomain = "quill";
     public static String adminDomain = "admin";
     public static String streamDomain = "stream1";
     public static String identityDomain = "identity";
-    public static String origin = "http://livefyre-cdn-dev.s3.amazonaws.com";
-    public static String referer = "http://livefyre-cdn-dev.s3.amazonaws.com/demos/lfep2-comments.html";
+    public static String origin = "https://livefyre-cdn-dev.s3.amazonaws.com";
+    public static String referer = "https://livefyre-cdn-dev.s3.amazonaws.com/demos/lfep2-comments.html";
     //	public static String networkId ="labs.fyre.co";
     private static String networkID = null;
 
