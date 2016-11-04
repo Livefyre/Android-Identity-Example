8d3d7e2b149fbd2cf35c6cd85a57306035d67b59
ramesh <ramesh.kvana@gmail.com>
2016-11-04 19:29:22 +0530

    build issues fixed, LF-23718-APIs Connect to Livefyre Servers over HTTPS by Default

--- streamhub-android-sdk/src/main/java/com/livefyre/streamhub_android_sdk/network/HttpClient.java
+++ streamhub-android-sdk/src/main/java/com/livefyre/streamhub_android_sdk/network/HttpClient.java
@@ -6,5 +6,5 @@ import com.loopj.android.http.AsyncHttpClient;
  * Created by jonathan on 7/29/13.
  */
 public class HttpClient {
-    public static AsyncHttpClient client = new AsyncHttpClient();
+    public static AsyncHttpClient client = new AsyncHttpClient(true, 80, 443);
 }
--- streamhub-android-sdk/src/main/java/com/livefyre/streamhub_android_sdk/util/Constant.java
+++ streamhub-android-sdk/src/main/java/com/livefyre/streamhub_android_sdk/util/Constant.java
@@ -9,6 +9,6 @@ public class Constant {
     public static final String SITE_ID = "291345";
     public static final String ARTICLE_ID = "designer-app-1444956522234";
     public static final class url {
-        public static final String BASE_URL = "http://admin.qa-blank.fyre.co";
+        public static final String BASE_URL = "https://admin.qa-blank.fyre.co";
     }
 }
